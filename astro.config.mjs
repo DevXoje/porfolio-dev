405a8966bf40628b37e6ba795f794e5bb20a3194
Xoje-Laptop <dawxoje@gmail.com>
2023-12-31 00:58:30 +0100

    Remove base URL from astro.config.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https//xojedev.github.io",
-  base: "/porfolio-dev",
   integrations: [tailwind()],
 });
