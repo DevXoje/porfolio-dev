eab47adc5c2b7e0c2cfd462696e21cfe82bc7017
Xoje-Laptop <dawxoje@gmail.com>
2023-12-31 01:30:38 +0100

    Add Husky pre-push and pre-commit hooks, update tsconfig.json and tailwind.config.mjs, and update astro.config.mjs, package.json, and .eslintrc.cjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://devxoje.github.io/porfolio-dev/",
-  base: "/porfolio-dev",
-  integrations: [tailwind()],
-});
+	site: 'https://devxoje.github.io/porfolio-dev/',
+	base: '/porfolio-dev',
+	integrations: [tailwind()]
+})
