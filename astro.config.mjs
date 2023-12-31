11499d9f14599b7278591c583940003e1f802171
Xoje-Laptop <dawxoje@gmail.com>
2023-12-31 11:53:49 +0100

    Update base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://devxoje.github.io/porfolio-dev/',
-	base: '/porfolio-dev',
+	base: '/',
 	integrations: [tailwind()]
 })
