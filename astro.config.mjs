fb2a307559924e7c01b5e83a4f4e7ce54a14e940
Xoje-Laptop <dawxoje@gmail.com>
2023-12-31 15:44:02 +0100

    Add language selector component and translate code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
+import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://devxoje.github.io/porfolio-dev/',
 	base: '/porfolio-dev',
-	integrations: [tailwind()]
+	integrations: [tailwind(), astroI18next()]
 })
